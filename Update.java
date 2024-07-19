95eeec0052e6321a7b166c159f545dc638be3e2c
Muhammad Younis <125958511+YounisJ@users.noreply.github.com>
2024-07-19 21:36:26 +0500

    Update Update.java

--- Update.java
+++ Update.java
@@ -1,21 +1,19 @@
-
 package dsa;
 
 import java.util.Scanner;
-
 public class Update {
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
         
         int[] arr={1,2,3,4,5,6};
-        
-        
+                
             System.out.println("before replace");
         for(int i=0; i<arr.length; i++)
         {
             System.out.println(arr[i]);
         }
-        
+
+        System.out.println();
         System.out.println("On which index you want to replace the number : ");
         int replaceIndex=input.nextInt();
         
