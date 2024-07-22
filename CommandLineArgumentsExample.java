0e7ffd487a7bf183405c545144ec53d61347c2f9
Muhammad Younis <125958511+YounisJ@users.noreply.github.com>
2024-07-22 16:09:10 +0500

    Update CommandLineArgumentsExample.java

--- CommandLineArgumentsExample.java
+++ CommandLineArgumentsExample.java
@@ -1,7 +1,8 @@
 public class CommandLineArgumentsExample {
     public static void main(String[] args) {
-        
+        System.out.println("");
         args[0]="hello";
+        
         
         
     }
